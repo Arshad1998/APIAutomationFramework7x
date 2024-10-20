0ab2516dcdda7dfc52a5d2f3d6775181f28c3c55
Arshad1998 <arshadshafeeq09@gmail.com>
2024-10-20 06:48:16 +0530

    September 29 files added

--- src/test/java/com/thetestingacademy/tests/integration/TestIntegrationFlowRetry.java
+++ src/test/java/com/thetestingacademy/tests/integration/TestIntegrationFlowRetry.java
@@ -14,6 +14,7 @@ import org.testng.annotations.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+
 @Test(retryAnalyzer = RetryAnalyzer.class)
 public class TestIntegrationFlowRetry extends BaseTest {
 // Integration Flow 1
